abd1161524b119a6117f1f876b33ad8357e4b179
Swaraj Roy <swarajroy2006@gmail.com>
2026-01-03 00:44:58 +0530

    fix gallery

--- assets/js/data/gallery.js
+++ assets/js/data/gallery.js
@@ -7,32 +7,32 @@
 export const galleryData = [
   {
     id: 1,
-    src: 'assets/img/about.png',
+    src: 'assets/img/gallery/3.jpeg',
     alt: 'Gallery moment 1'
   },
   {
     id: 2,
-    src: 'assets/img/work1.jpg',
+    src: 'assets/img/gallery/1.jpeg',
     alt: 'Gallery moment 2'
   },
   {
     id: 3,
-    src: 'assets/img/work2.png',
+    src: 'assets/img/gallery/2.jpeg',
     alt: 'Gallery moment 3'
   },
   {
     id: 4,
-    src: 'assets/img/work3.png',
+    src: 'assets/img/gallery/4.jpeg',
     alt: 'Gallery moment 4'
   },
   {
     id: 5,
-    src: 'assets/img/soon.jpg',
+    src: 'assets/img/about.png',
     alt: 'Gallery moment 5'
   },
   {
     id: 6,
-    src: 'assets/img/about.png',
+    src: 'assets/img/soon.jpg',
     alt: 'Gallery moment 6'
   }
 ];
